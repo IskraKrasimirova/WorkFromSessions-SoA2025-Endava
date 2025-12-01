275bea592f3ebb88c76bad57104765f8beb9be0c
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-12-01 18:06:46 +0200

    Add generic method for sorting array descending with built-in methods

---
+++ Loops/SortArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SortArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { -3, 5, 987654321, -2, int.MinValue, 1, 4, int.MaxValue, 0, -123456789 };
+            double[] doubleArray = { 3.1, 4.1, double.MaxValue, 5.9, 2.6, double.MinValue, 5.3 };
+            string[] stringArray = { "gama", "banana", "apple", "cherry", "beta", "delta", "date", "alfa", string.Empty };
+
+            SortArrayDesc(intArray);
+            SortArrayDesc(doubleArray);
+            SortArrayDesc(stringArray);
+        }
+
+        private static void SortArrayDesc<T>(T[] array) where T : IComparable<T>
+        {
+            Array.Sort<T>(array);
+            Array.Reverse(array);
+            Console.WriteLine("Sorted descending array: " + string.Join(", ", array));
+        }
+    }
+}

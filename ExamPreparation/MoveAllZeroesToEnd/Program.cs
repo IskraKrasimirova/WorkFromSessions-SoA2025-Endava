94580982f606f64121474c48a7b903adf5b7941a
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-12-11 23:53:21 +0200

    Add task for moving all zero numbers at the end of array

---
+++ ExamPreparation/MoveAllZeroesToEnd/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MoveAllZeroesToEnd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int index = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    numbers[index] = numbers[i];
+                    numbers[i] = 0;
+                    index++;
+                }
+            }
+
+            //// Another decision by swapping elements (tuple swap)
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] != 0)
+            //    {
+            //        (numbers[index], numbers[i]) = (numbers[i], numbers[index]);;
+            //        index++;
+            //    }
+            //}
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
+//4.Task 8 — Move All Zeroes to the End (In-Place)
+// Description:
+//Given an array of N integers, move all the zeroes
+//to the end of the array, without changing the order of the non-zero elements.
+//The operation should be performed in place by swapping elements.
+//Print the updated array.
+// Example:
+// Input:
+//7
+//0 1 0 3 12 0 5
+// Output:
+//1 3 12 5 0 0 0

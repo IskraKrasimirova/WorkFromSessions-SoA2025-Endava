cf770fb9d536fef66c49983162522c9091b051e6
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2025-12-13 21:59:08 +0200

    Add decisions with LINQ, Lists, Array, but not in-place

---
+++ ExamPreparation/MoveAllZeroesToEndLINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MoveAllZeroesToEndLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LINQ decision is not "in-place"
+            int[] numbers = Console.ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            numbers = numbers
+                .Where(x => x != 0)
+                .Concat(numbers.Where(x => x == 0))
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ ExamPreparation/MoveAllZeroesToEndWithList/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MoveAllZeroesToEndWithList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()!
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            //// Decision with Lists, not "in-place"
+            //List<int> nonZeroes = new List<int>();
+            //List<int> zeroes = new List<int>();
+
+            //foreach (int number in numbers) 
+            //{
+            //    if (number == 0)
+            //    {
+            //        zeroes.Add(number);
+            //    }
+            //    else
+            //    {
+            //        nonZeroes.Add(number);
+            //    }
+            //}
+
+            //var result = nonZeroes.Concat(zeroes).ToArray();
+
+            // Decision with array, not "in-place"
+            int[] result = new int[numbers.Length];
+            int index = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    result[index++] = numbers[i];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
